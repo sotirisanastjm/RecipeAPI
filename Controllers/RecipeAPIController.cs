aea5a0127ae4eab303b754e283fb2d5b2a47d2f7
Sotirios Anastasiadis <64400907+sotirisanastjm@users.noreply.github.com>
2023-02-22 17:33:37 +0200

    Creating RecipeAPIController

---
+++ Controllers/RecipeAPIController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeAPI.Controllers
+{
+    [ApiController]
+    public class RecipeAPIController : ControllerBase
+    {
+        public Recipe() {}
+    }
+}
