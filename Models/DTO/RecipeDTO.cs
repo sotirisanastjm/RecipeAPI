39c331e11cdcd828a9199f0da302b68feed5ade1
Sotirios Anastasiadis <64400907+sotirisanastjm@users.noreply.github.com>
2023-02-22 21:14:22 +0200

    RecipeDTO
    Modifing some parametres for RecipeDTO

--- Models/DTO/RecipeDTO.cs
+++ Models/DTO/RecipeDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace RecipeAPI.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeAPI.Models.DTO
 {
     public class RecipeDTO
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(40)]
         public string Name { get; set; }
     }
 }
