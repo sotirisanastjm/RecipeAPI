49c3887d0f9e683b9c362996b5d42324797905f2
Sotirios Anastasiadis <64400907+sotirisanastjm@users.noreply.github.com>
2023-02-23 22:41:33 +0200

    downloading entityframework

--- Models/DTO/RecipeDTO.cs
+++ Models/DTO/RecipeDTO.cs
@@ -10,5 +10,7 @@ namespace RecipeAPI.Models.DTO
         public string Name { get; set; }
         public string Description { get; set; }
         public string Ingredients { get; set; }
+        public string Execution { get; set; }
+        public string ImageURL { get; set; }
     }
 }
--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -1,9 +1,14 @@
-﻿namespace RecipeAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeAPI.Models
 {
     public class Recipe
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Ingredients { get; set; }
+        public string Execution { get; set; }
+        public string ImageURL { get; set; }
     }
 }
